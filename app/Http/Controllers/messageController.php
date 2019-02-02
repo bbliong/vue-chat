d65a420df5fff176f9f43f01a7067977fdb3f015
bbliong <ariaelhamidy@gmail.com>
2019-02-02 13:42:37 +0700

    Update baru

--- app/Http/Controllers/messageController.php
+++ app/Http/Controllers/messageController.php
@@ -2,26 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Models\Files;
 use App\Models\Message;
+use App\Models\Files;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class messageController extends Controller
 {
     public function index(){
         //Geting All Messages
-        $message = Message::with(['File', 'User'])->get();
+
+        $message = Message::with(['File'])->get();
         return $message;
     }
 
     public function show(){
       return view('chat');
-    }
-
-    public function user(){
-      $user = Auth::user();
-      return $user;
     }
 }
