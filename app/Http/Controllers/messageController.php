d6d2c3d1f08283374d3b131d78b8c271747d682c
bbliong <ariaelhamidy@gmail.com>
2019-02-02 13:31:59 +0700

    Persiapan untuk install laravel-echo

--- app/Http/Controllers/messageController.php
+++ app/Http/Controllers/messageController.php
@@ -2,20 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Message;
+use App\Models\User;
 use App\Models\Files;
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class messageController extends Controller
 {
     public function index(){
         //Geting All Messages
-
-        $message = Message::with(['File'])->get();
+        $message = Message::with(['File', 'User'])->get();
         return $message;
     }
 
     public function show(){
       return view('chat');
+    }
+
+    public function user(){
+      $user = Auth::user();
+      return $user;
     }
 }
