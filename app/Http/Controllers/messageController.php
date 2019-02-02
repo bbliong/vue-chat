b866fe2827f322c0bbf6f90c5d2bcffefe5ba98f
bbliong <ariaelhamidy@gmail.com>
2019-02-02 10:00:44 +0700

    Update message Controller

--- app/Http/Controllers/messageController.php
+++ app/Http/Controllers/messageController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
+use App\Models\Files;
 use Illuminate\Http\Request;
 
 class messageController extends Controller
 {
-    //
+    public function index(){
+        //Geting All Messages
+
+        $message = Message::with(['File'])->get();
+        return $message;
+    }
 }
