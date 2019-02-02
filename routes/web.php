920e6dce94ba1c8608077b614c20c77640dd82fc
bbliong <ariaelhamidy@gmail.com>
2019-02-02 09:43:22 +0700

    Model

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/chat', 'MessageController@index')->name('chat');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
